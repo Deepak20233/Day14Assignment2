eb52793ead11d8d184c7cb47e412ef2f0bde60b5
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-27 20:03:57 +0530

    Added:Stack problem

---
+++ StackUC1.java
@@ -0,0 +1,16 @@
+package com.java;
+import java.util.Stack;
+public class StackUC1 {
+	 public static void main(String[] args) {
+	Stack<Integer> stack = new Stack<>();
+    
+    // Push elements onto the stack
+    stack.push(56);
+    stack.push(30);
+    stack.push(70);
+    
+    // Print the elements in the stack
+    System.out.println("Elements in stack: " + stack);
+}
+}
+
